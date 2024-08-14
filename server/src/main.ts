11d5155b3ba827003b356809390175aab74f0ebb
quanghuong <quanghuong.dev@gmail.com>
2024-08-14 13:43:15 +0700

    Set cooki parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /* Env */
   const configService = app.get(ConfigService);
+  /* Middlewares */
+  app.use(cookieParser());
+
   /* Listen */
   const port = <number>configService.get<number>('PORT', { infer: true });
   await app.listen(port, () => {
